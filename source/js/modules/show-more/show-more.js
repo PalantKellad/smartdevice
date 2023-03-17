2bfac76f5ceb01702aa11dd22336b55dc0d95c95
Vladimir Trofimov <i_am@palant.org>
2023-03-17 17:18:12 +0300

    Fixes bugs after review

--- source/js/modules/show-more/show-more.js
+++ source/js/modules/show-more/show-more.js
@@ -1,24 +1,28 @@
 function showMore() {
   const element = document.querySelector('.about__wrapper');
   const button = document.querySelector('button[data-more-button]');
-  const elementStyles = window.getComputedStyle(element);
-  const elementPropertyLH = elementStyles.getPropertyValue('line-height');
-  const lineHeight = parseInt(elementPropertyLH, 10);
-  const linesCount = getComputedStyle(element).getPropertyValue('--visible-lines-count');
-  const visibleHeight = linesCount * lineHeight;
-  element.style.maxHeight = `${visibleHeight}px`;
+  if (element) {
+    const elementStyles = window.getComputedStyle(element);
+    const elementPropertyLH = elementStyles.getPropertyValue('line-height');
+    const lineHeight = parseInt(elementPropertyLH, 10);
+    const linesCount = getComputedStyle(element).getPropertyValue('--visible-lines-count');
+    const visibleHeight = linesCount * lineHeight;
 
-  button.addEventListener('click', function () {
-    if (element.style.maxHeight === `${visibleHeight}px`) {
-      // Если элемент свернут, то разворачиваем его
-      element.style.maxHeight = '100%';
-      button.textContent = 'Свернуть';
-    } else {
-      // Если элемент развернут, то сворачиваем его
+    if (button) {
       element.style.maxHeight = `${visibleHeight}px`;
-      button.textContent = 'Подробнее';
+      button.addEventListener('click', function () {
+        if (element.style.maxHeight === `${visibleHeight}px`) {
+          // Если элемент свернут, то разворачиваем его
+          element.style.maxHeight = '100%';
+          button.textContent = 'Свернуть';
+        } else {
+          // Если элемент развернут, то сворачиваем его
+          element.style.maxHeight = `${visibleHeight}px`;
+          button.textContent = 'Подробнее';
+        }
+      });
     }
-  });
+  }
 }
 
 export {showMore};
